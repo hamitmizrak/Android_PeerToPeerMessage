a93edb7aa0efe91208b22bfdf395d8cdbb11d567
hamitmizrak <hamitmizrak@gmail.com>
2022-12-17 22:01:33 +0300

    Revize project added

--- app/src/main/java/com/hamitmizrak/peertopeermessage/MainActivity.java
+++ app/src/main/java/com/hamitmizrak/peertopeermessage/MainActivity.java
@@ -1,7 +1,7 @@
 package com.hamitmizrak.peertopeermessage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
