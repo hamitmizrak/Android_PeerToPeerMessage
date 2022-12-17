c3f0bebd6cd18203c19fa717f6ae435a32b1e9ae
hamitmizrak <hamitmizrak@gmail.com>
2022-12-17 21:10:54 +0300

    Initial commit

---
+++ app/src/main/java/com/hamitmizrak/peertopeermessage/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak.peertopeermessage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //ONCREATE
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //start codes
+
+    } //ends codes
+}// class MainActivity
