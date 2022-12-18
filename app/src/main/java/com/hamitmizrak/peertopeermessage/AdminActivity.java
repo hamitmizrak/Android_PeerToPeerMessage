698f248d32513ce5bb83a083545863c9f4af4f42
hamitmizrak <hamitmizrak@gmail.com>
2022-12-18 21:54:09 +0300

    RegisterActivity,Login

--- app/src/main/java/com/hamitmizrak/peertopeermessage/AdminActivity.java
+++ app/src/main/java/com/hamitmizrak/peertopeermessage/AdminActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AdminActivity extends AppCompatActivity {
+    //global variable
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_admin);
-}
+    //ONCREATE
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
 }
--- app/src/main/java/com/hamitmizrak/peertopeermessage/RegisterActivity.java
+++ app/src/main/java/com/hamitmizrak/peertopeermessage/RegisterActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
+    //global variable
 
+    //ONCREATE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
