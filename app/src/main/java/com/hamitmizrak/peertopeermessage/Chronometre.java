ddf038f2845778cfdc0beccb9ad02759fd865120
hamitmizrak <hamitmizrak@gmail.com>
2023-01-07 20:23:12 +0300

    Chronometre

--- app/src/main/java/com/hamitmizrak/peertopeermessage/Chronometre.java
+++ app/src/main/java/com/hamitmizrak/peertopeermessage/Chronometre.java
@@ -3,14 +3,50 @@ package com.hamitmizrak.peertopeermessage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class Chronometre extends AppCompatActivity {
     //Global Variable
-
+    private Button chronometreStartId;
+    private Button chronometrePauseId;
+    private Button chronometreRestartId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chronometre);
-    }
-}
+        //start
+        chronometreStartId = findViewById(R.id.chronometreStartId);
+        chronometrePauseId = findViewById(R.id.chronometrePauseId);
+        chronometreRestartId = findViewById(R.id.chronometreRestartId);
+
+        Chronometer chronometer = findViewById(R.id.chronometreId);
+
+        chronometreStartId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.start();
+            }
+        });
+
+        chronometrePauseId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.stop();
+            }
+        });
+
+
+        chronometreRestartId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+
+        //end
+    } //onCreate
+} //End Class
